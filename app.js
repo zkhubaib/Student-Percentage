2b2854c13fb6b5e5399ebcb65d8a641d04f62265
zkhubaib <148073434+zkhubaib@users.noreply.github.com>
2023-11-29 05:23:25 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+var a = +prompt("Physics");
+var b = +prompt("Math");
+var c = +prompt("English");
+var d = +prompt("C.Science");
+var e = +prompt("Urdu");
+
+var f = 460;
+var percentage = ((a+b+c+d+e)/f)*100;
+
+alert(f)
+alert(percentage);
